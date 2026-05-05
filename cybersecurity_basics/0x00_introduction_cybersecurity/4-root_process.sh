1e59cf34e1300837a136dc47a9eceedc9fd36658
scarlettstoat <surayatessa@gmail.com>
2026-05-05 18:27:49 +0100

    Fixed using ps aux

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u $1 -o user,pid,pcpu,pmem,vsz,rss,tty,stat,start,start,time,cmd --no-headers | grep -v "^\S* *\S* *\S* *\S* *0 *0"
+ps aux | grep $1 | grep -v "^\S* *\S* *\S* *\S* *0 *0"
