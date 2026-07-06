93fe96ccae5888a079519521af47964cefc1c57c
scarlettstoat <surayatessa@gmail.com>
2026-07-06 08:48:53 -0400

    Add script retrieves and displays Header + ignore pyscache

---
+++ scripting_cyber/0x01-python_scripting/3-http_headers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash python3
+
+import requests
+
+
+def get_http_headers(url):
+    try:
+        response = requests.get(url)
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers)
+        }
+    except requests.exceptions.RequestException:
+        return None
